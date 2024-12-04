3666d402523d53c71745d136429069f86f3bdc75
Alexandra Babis <aebabis@gmail.com>
2024-12-03 19:08:03 -0500

    Adding weather plugin and install script (script not tested)

---
+++ new-machine.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Command dependencies
+apps="stow go fzf"
+
+if command -v brew 2>&1 >/dev/null; then
+  brew install $apps
+elif command -v apt 2>&1 >/dev/null; then
+  sudo apt install $apps
+else
+  >&2 echo "Installer not detected"
+fi
+
+# Plugin and repo dependencies
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+./link.sh
