b85cb678ac96a54b09c57aea7c00c8fda402f23e
Alexandra Babis <aebabis@gmail.com>
2025-01-03 14:55:35 -0500

    Trying new LSP config. Also fixes autocomplete

--- nvim/.config/nvim/lua/plugins/autocomplete.lua
+++ nvim/.config/nvim/lua/plugins/autocomplete.lua
@@ -1,31 +1,101 @@
 return {
   'hrsh7th/nvim-cmp',
-  event = {'InsertEnter'},
   dependencies = {
+    'hrsh7th/cmp-nvim-lsp',
     'hrsh7th/cmp-buffer',
+    -- 'hrsh7th/cmp-path',
+    --'hrsh7th/cmp-cmdline',
+    -- Not sure that I need these ^
+    -- Snippets
+    'L3MON4D3/LuaSnip',
+    'rafamadriz/friendly-snippets',
+    'saadparwaiz1/cmp_luasnip',
+    -- Format help (adds type and icon to dropdown)
+    'onsails/lspkind.nvim'
   },
+  lazy = true,
+  event = 'InsertEnter',
   config = function()
+    -- Setup completion
     local cmp = require('cmp')
+    local lspkind = require('lspkind')
+    local cmp_select = { behavior = cmp.SelectBehavior.Select }
+
+    local has_words_before = function()
+      unpack = unpack or table.unpack
+      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+      return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
+    end
 
     cmp.setup({
-      sources = {
-        {name = 'nvim_lsp'},
+      formatting = {
+        format = lspkind.cmp_format({
+          mode = 'symbol_text', -- show only symbol annotations
+          maxwidth = {
+            -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+            -- can also be a function to dynamically calculate max width such as
+            -- menu = function() return math.floor(0.45 * vim.o.columns) end,
+            menu = 50,              -- leading text (labelDetails)
+            abbr = 50,              -- actual suggestion item
+          },
+          ellipsis_char = '...',    -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+          show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+
+          -- The function below will be called before any actual modifications from lspkind
+          -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+          before = function(entry, vim_item)
+            vim_item.menu = entry.source.name
+            return vim_item
+          end
+        })
       },
-      -- TODO: Figure out why none of these do anything
-      mapping = cmp.mapping.preset.insert({
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-g>'] = cmp.mapping.complete(),
-        ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-d>'] = cmp.mapping.scroll_docs(4),
-      }),
+      sources = {
+        { name = 'buffer' }, -- cmp-buffer
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' } -- cmp_luasnip
+      },
       snippet = {
         expand = function(args)
-          vim.snippet.expand(args.body)
+          require('luasnip').lsp_expand(args.body)
         end,
-      }, {
-        { name = 'buffer' },
-      }
+      },
+      mapping = cmp.mapping.preset.insert({
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-k>'] = cmp.mapping.select_prev_item(cmp_select),
+        ['<C-j>'] = cmp.mapping.select_next_item(cmp_select),
+        ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<Tab>'] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            -- TODO: Figure out how to make callout disappear faster,
+            -- or switch to using cmp.confirm({ select = true }) to select-and-close
+            cmp.select_next_item()
+          elseif vim.snippet.active({ direction = 1 }) then
+            vim.schedule(function()
+              vim.snippet.jump(1)
+            end)
+          elseif has_words_before() then
+            cmp.complete()
+          else
+            fallback()
+          end
+        end, { 'i', 's' }),
+        ['<S-Tab>'] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif vim.snippet.active({ direction = -1 }) then
+            vim.schedule(function()
+              vim.snippet.jump(-1)
+            end)
+          else
+            fallback()
+          end
+        end, { 'i', 's' }),
+      }),
     })
+
+    require('luasnip.loaders.from_vscode').lazy_load()
   end
-}
+};
 
--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,70 +1,174 @@
-return {{
-  'neovim/nvim-lspconfig',
-  cmd = {'LspInfo', 'LspInstall', 'LspStart'},
-  event = {'BufReadPre', 'BufNewFile'},
-  dependencies = {
-    {'hrsh7th/cmp-nvim-lsp'},
-    {'williamboman/mason.nvim'},
-    {'williamboman/mason-lspconfig.nvim'},
-  },
-  init = function(PluginSpec, opts)
-    -- Reserve a space in the gutter
-    -- This will avoid an annoying layout shift in the screen
-    vim.opt.signcolumn = 'yes'
-  end,
-  config = function()
-    -- Add cmp_nvim_lsp capabilities settings to lspconfig
-    -- This should be executed before you configure any language server
-    local lspconfig_defaults = require('lspconfig').util.default_config
-    local capabilities = vim.tbl_deep_extend(
-      'force',
-      lspconfig_defaults.capabilities,
-      require('cmp_nvim_lsp').default_capabilities()
-    )
-    lspconfig_defaults.capabilities = capabilities
+return {
+  {
+    -- LSP Server Management Application
+    'williamboman/mason.nvim',
+    lazy = false,
+    opts = {}
+  }, {
+    -- LSP
+    'neovim/nvim-lspconfig',
+    lazy = true,
+    cmd = { 'LspInfo', 'LspInstall', 'LspStart' },
+    event = { 'BufReadPre', 'BufNewFile' },
+    init = function()
+      vim.opt.signcolumn = 'yes'
+    end,
+    config = function()
+      -- LspAttach is where you enable features that only work
+      -- if there is a language server active in the file
+      vim.api.nvim_create_autocmd('LspAttach', {
+        desc = 'LSP actions',
+        ---@diagnostic disable-next-line: unused-local
+        callback = function(client, buf)
+          local opts = { buffer = buf, remap = false }
 
-    -- This is where you enable features that only work
-    -- if there is a language server active in the file
-    vim.api.nvim_create_autocmd('LspAttach', {
-      desc = 'LSP actions',
-      callback = function(event)
-        local opts = {buffer = event.buf}
+          vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
+          vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, opts)
+          vim.keymap.set('n', '<leader>vws', function() vim.lsp.buf.workspace_symbol() end, opts)
+          vim.keymap.set('n', '<leader>vd', function() vim.diagnostic.open_float() end, opts)
+          vim.keymap.set('n', '<leader>vl', function() vim.diagnostic.setqflist() end, opts)
+          vim.keymap.set('n', '[d', function() vim.diagnostic.goto_next() end, opts)
+          vim.keymap.set('n', ']d', function() vim.diagnostic.goto_prev() end, opts)
+          vim.keymap.set('n', '<leader>vca', function() vim.lsp.buf.code_action() end, opts)
+          vim.keymap.set('n', '<leader>vrr', function() vim.lsp.buf.references() end, opts)
+          vim.keymap.set('n', '<leader>vrn', function() vim.lsp.buf.rename() end, opts)
+          -- Ensure that ts_ls does not format. Stupid
+          vim.keymap.set({ 'n', 'v' }, '<leader>vf',
+            function() vim.lsp.buf.format({ filter = function(client) return client.name ~= 'ts_ls' end }) end, opts)
 
-        vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-        vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-        vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-        vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-        vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-        vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-        vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-        vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-        vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
-        vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
-      end,
-    })
+          vim.keymap.set('i', '<C-h>', function() vim.lsp.buf.signature_help() end, opts)
+        end,
+      })
+    end
+  }, {
+    -- LSP Server Configuration
+    'williamboman/mason-lspconfig.nvim',
+    lazy = true,
+    event = { 'BufReadPre', 'BufNewFile' },
+    config = function()
+      local masonConfig = require('mason-lspconfig')
 
-    require('mason-lspconfig').setup({
-      ensure_installed = {},
-      handlers = {
-        function(server_name)
-	  require('lspconfig')[server_name].setup({})
-	end,
-
-        ['lua_ls'] = function()
-          local lspconfig = require("lspconfig")
-          lspconfig.lua_ls.setup {
-            capabilities = capabilities,
-            settings = {
-              Lua = {
-                diagnostics = {
-                  globals = { "vim" },
+      local capabilities = vim.tbl_deep_extend(
+        'force',
+        require('lspconfig').util.default_config.capabilities,
+        require('cmp_nvim_lsp').default_capabilities()
+      )
+      masonConfig.setup({
+        ensure_installed = { 'ts_ls', 'eslint', 'lua_ls', 'yamlls', 'jsonls' },
+        automatic_installation = true,
+        handlers = {
+          function(server_name)
+            require('lspconfig')[server_name].setup({ capabilities = capabilities })
+          end,
+          jsonls = function()
+            local opts = {
+              capabilities = capabilities,
+              filetypes = { 'json', 'jsonc' },
+              settings = {
+                json = {
+                  schemas = {
+                    {
+                      fileMatch = { 'package.json' },
+                      url = 'https://json.schemastore.org/package.json'
+                    },
+                    {
+                      fileMatch = { 'tsconfig*.json' },
+                      url = 'https://json.schemastore.org/tsconfig.json'
+                    },
+                    {
+                      fileMatch = {
+                        '.prettierrc',
+                        '.prettierrc.json',
+                        'prettier.config.json'
+                      },
+                      url = 'https://json.schemastore.org/prettierrc.json'
+                    },
+                    {
+                      fileMatch = { '.eslintrc', '.eslintrc.json' },
+                      url = 'https://json.schemastore.org/eslintrc.json'
+                    },
+                    {
+                      fileMatch = { '.babelrc', '.babelrc.json', 'babel.config.json' },
+                      url = 'https://json.schemastore.org/babelrc.json'
+                    },
+                    {
+                      fileMatch = { 'lerna.json' },
+                      url = 'https://json.schemastore.org/lerna.json'
+                    },
+                    {
+                      fileMatch = { 'now.json', 'vercel.json' },
+                      url = 'https://json.schemastore.org/now.json'
+                    },
+                    {
+                      fileMatch = {
+                        '.stylelintrc',
+                        '.stylelintrc.json',
+                        'stylelint.config.json'
+                      },
+                      url = 'http://json.schemastore.org/stylelintrc.json'
+                    }
+                  }
                 }
               }
             }
-          }
-        end,
-      }
-    })
-  end,
-}}
+            require('lspconfig').jsonls.setup(opts)
+          end,
+          lua_ls = function()
+            -- Ensure that nvim lsp recognizes the vim global object
+            require('lspconfig').lua_ls.setup({
+              capabilities = capabilities,
+              settings = {
+                Lua = {
+                  runtime = {
+                    version = 'LuaJIT'
+                  },
+                  diagnostics = {
+                    globals = { 'vim' },
+                  },
+                  workspace = {
+                    checkThirdParty = false,
+                    library = {
+                      vim.env.VIMRUNTIME,
+                      '~/.local/share/nvim/lazy'
+                    }
+                  }
+                }
+              }
+            })
+          end,
+          efm = function()
+            -- got most of this config from:
+            -- https://github.com/lukas-reineke/dotfiles/blob/master/vim/lua/efm/prettier.lua
+            local prettier = {
+              formatCommand =
+              [[$([ -n "$(command -v node_modules/.bin/prettier)" ] && echo "node_modules/.bin/prettier" || echo "prettier") --stdin-filepath ${INPUT} ]],
+              formatStdin = true,
+            }
+
+            local languages = {
+              typescript = { prettier },
+              typescriptreact = { prettier },
+              javascript = { prettier }
+            }
+
+            local opts = {
+              capabilities = capabilities,
+              filetypes = vim.tbl_keys(languages),
+              settings = {
+                rootMarkers = { '.git/' },
+                languages = languages,
+              },
+              init_options = {
+                documentFormatting = true
+              },
+            }
+
+            require('lspconfig').efm.setup(opts)
+          end,
+        }
+      })
+    end
+  }
+}
+
 
