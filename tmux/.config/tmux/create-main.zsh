9bc44c69d1a13dfd025ac81264b0d29a7094823a
Alexandra Babis <aebabis@gmail.com>
2025-01-21 14:58:23 -0700

    Refactoring auto-tmux script

--- tmux/.config/tmux/create-main.zsh
+++ tmux/.config/tmux/create-main.zsh
@@ -11,23 +11,17 @@ then
   # window 0 is the meta window, for writing dotfiles
   tmux rename-window -t 0 '自然'
   tmux send-keys -t '自然' 'nvim ~/Projects/dotfiles' C-m
-  tmux split-window -v -t '自然' -p 20
-  tmux send-keys -t '自然' 'cd ~/Projects/dotfiles' C-m C-l
-  tmux select-pane -U
+  tmux split-window -t '自然' -v -p 20 -c "$HOME/$projects/dotfiles" -d
 
   # window 1 is for current project or primary focus
   tmux new-window -t $session:1 -n 'project'
   tmux send-keys -t 'project' "cd $projects" C-m C-l
-  tmux split-window -t 'project' -v -p 20
-  tmux send-keys -t 'project' "cd $projects" C-m C-l
-  tmux select-pane -U
+  tmux split-window -t 'project' -v -p 20 -c "$HOME/$projects" -d
 
   # window 2 is support window for window 1 (e.g. dev server, scrumboard)
   tmux new-window -t $session:2 -n 'project-aux'
   tmux send-keys -t 'project-aux' "cd $projects" C-m C-l
-  tmux split-window -t 'project-aux' -h
-  tmux send-keys -t 'project-aux' "cd $projects" C-m C-l
-  tmux select-pane -L
+  tmux split-window -t 'project-aux' -h -c "$HOME/$projects" -d
 
   # create a 3rd window without naming it
   tmux new-window -t $session:3
