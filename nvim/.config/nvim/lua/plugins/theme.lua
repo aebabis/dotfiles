1e53c0ef1a29b5ede70b0e3eaea15b02457597ff
Alexandra Babis <aebabis@gmail.com>
2025-09-08 11:34:49 -0400

    light/dark toggle for vim and ghostty

---
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -0,0 +1,15 @@
+return {
+  'f-person/auto-dark-mode.nvim',
+  opts = {
+    set_dark_mode = function()
+      vim.api.nvim_set_option_value('background', 'dark', {})
+      vim.cmd.colorscheme('nightfox')
+    end,
+    set_light_mode = function()
+      vim.api.nvim_set_option_value('background', 'light', {})
+      vim.cmd.colorscheme('dayfox')
+    end,
+    update_interval = 3000,
+    fallback = "dark"
+  }
+}
