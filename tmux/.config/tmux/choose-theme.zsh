51badae1ace03946ec6cb58ca5faf875f7d7fcb8
Alexandra Babis <aebabis@gmail.com>
2025-09-10 13:37:51 -0400

    tmux support for auto-topbar-theme

---
+++ tmux/.config/tmux/choose-theme.zsh
@@ -0,0 +1,9 @@
+# Import appearance-detection functions
+source ~/.shell-functions
+
+if [[ "$(os_appearance)" == "dark" ]]
+then
+  tmux source-file ~/.config/tmux/theme.dark.tmux.conf
+else
+  tmux source-file ~/.config/tmux/theme.light.tmux.conf
+fi
