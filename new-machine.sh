580916348200e7ae5cc2b65c659dfa5c9b78ef53
aebabis <acbabis@gmail.com>
2026-01-22 14:07:57 -0700

    Improving installer

--- new-machine.sh
+++ new-machine.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
 # Command dependencies
-apps="stow go fzf tealdeer"
+apps="stow fzf tealdeer"
 
 if command -v brew 2>&1 >/dev/null; then
-  brew install $apps
+  brew install "$apps go thefuck"
+  cargo install --locked tree-sitter-cli
 elif command -v apt 2>&1 >/dev/null; then
-  sudo apt install $apps
+  sudo apt install "$apps golang-go tree-sitter-cli"
+  sudo apt install "python3-dev python3-pip python3-setuptools"
+  pip3 install thefuck --user
 else
   >&2 echo "Installer not detected"
 fi
