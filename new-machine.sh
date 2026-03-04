fc08030e51f51ab625aa7dd0be3791eda686330d
Alexandra Babis <aebabis@gmail.com>
2026-03-04 16:47:08 -0500

    rude

--- new-machine.sh
+++ new-machine.sh
@@ -3,16 +3,19 @@
 # Run from dotfiles root so link.sh and clone target work regardless of cwd
 cd "$(dirname "$0")" || exit 1
 
+rude=$(echo gurshpx | tr "[a-z]" "[n-za-m]")
+echo $rude
+
 # Command dependencies
 apps="stow fzf tealdeer"
 
 if command -v brew 2>&1 >/dev/null; then
-  brew install $apps go thefuck
+  brew install $apps go $rude
   cargo install --locked tree-sitter-cli
 elif command -v apt 2>&1 >/dev/null; then
   sudo apt install $apps golang-go tree-sitter-cli
   sudo apt install python3-dev python3-pip python3-setuptools
-  pip3 install thefuck --user
+  pip3 install $rude --user
 else
   >&2 echo "Installer not detected"
 fi
