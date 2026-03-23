c6f3e44911f91dd4da64c57086cb4827760ed959
Alexandra Babis <aebabis@gmail.com>
2026-03-23 14:01:26 -0400

    Fixing bugs in tmux scripts

--- tmux/.config/tmux/create-main.zsh
+++ tmux/.config/tmux/create-main.zsh
@@ -1,18 +1,17 @@
 #!/bin/zsh
 
 session=main
-session_exists=$(tmux list-sessions | grep $session)
 
-if [ "$session_exists" = "" ]
+if ! tmux has-session -t "$session" 2>/dev/null
 then
-  projects=$(ls | egrep '^.rojects$' | head -1)
+  projects=$(ls ~ | egrep '^.rojects$' | head -1)
   tmux new-session -d -s $session
 
   # window 0 is the meta window, for writing dotfiles
   tmux run-shell -t "$session" "$(dirname "$0")/open-project.zsh -w -n '自然' '$HOME/$projects/dotfiles'"
 
   # window 1 is for current project or primary focus
-  tmux run-shell -t "$session" "$(dirname "$0")/open-project.zsh -n 'project' '$HOME/$projects'"
+  tmux run-shell -t "$session" "$(dirname "$0")/open-project.zsh -n 'project' '$HOME/$projects' -c 'clear; echo \"Press Ctrl+] to set NvimTree root\"'"
 
   # window 2 is support window for window 1 (e.g. dev server, scrumboard)
   tmux new-window -t $session:2 -n 'project-aux'
@@ -20,7 +19,7 @@ then
   tmux split-window -t 'project-aux' -h -c "$HOME/$projects" -d
 
   # create a 3rd window without naming it
-  tmux new-window -t $session:3
+  tmux new-window -c ~ -t $session:3
 
   # attach
   tmux attach-session -t $session:1
--- tmux/.config/tmux/open-project.zsh
+++ tmux/.config/tmux/open-project.zsh
@@ -1,13 +1,14 @@
 #!/bin/zsh
-# Usage: ./open-project.zsh [-w] [-c command1] [-c command2] [-n name] <dir>
+# Usage: ./open-project.zsh [-w] [-s session] [-c command1] [-c command2] [-n name] <dir>
 #   -w  configure the current window instead of creating a new one
+#   -s  tmux session name (defaults to current session via display-message)
 
 main() {
-  local session=$(tmux display-message -p "#S")
-
   # parse options
-  local -a c_vals n_vals w_flag
-  zparseopts -D -E -- c+:=c_vals n:=n_vals w=w_flag
+  local -a c_vals n_vals s_vals w_flag
+  zparseopts -D -E -- c+:=c_vals n:=n_vals s:=s_vals w=w_flag
+
+  local session="${s_vals[2]:-$(tmux display-message -p "#S")}"
   local -a commands=(${c_vals:#-c})  # c_vals is (-c val -c val ...), strip the flag entries
   local name="${n_vals[2]}"
   local dir="$1"
@@ -41,6 +42,7 @@ main() {
   local bottom_pane=$(tmux split-window -t "$session:$window" -v -p 20 -c "$dir" -P -F "#{pane_id}")
 
   # run nvim in the top pane
+  tmux send-keys -t "$session:$window.0" "cd '$dir'" C-m
   tmux send-keys -t "$session:$window.0" "nvim ." C-m
 
   # split bottom pane into horizontal panes, running a command in each one
