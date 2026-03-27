2ceebe379bb53170a486be4e44e4065fcfba7a4b
Alexandra Babis <aebabis@gmail.com>
2026-03-27 16:58:02 -0400

    diffview

--- nvim/.config/nvim/lua/plugins/git-integration.lua
+++ nvim/.config/nvim/lua/plugins/git-integration.lua
@@ -1,16 +1,28 @@
 return {
-  'tpope/vim-fugitive', -- FUGITIVE
-  lazy = true,
-  event = { 'BufReadPre', 'BufNewFile' },
-  keys = {
-    { "<leader>gs" }
-  },
-  config = function()
-    -- Git status
-    vim.keymap.set('n', '<leader>gs', vim.cmd.Git);
+  {
+    'tpope/vim-fugitive', -- FUGITIVE
+    lazy = true,
+    event = { 'BufReadPre', 'BufNewFile' },
+    keys = {
+      { "<leader>gs" }
+    },
+    config = function()
+      -- Git status
+      vim.keymap.set('n', '<leader>gs', vim.cmd.Git);
 
-    -- Git blame
-    vim.keymap.set('n', '<leader>gb', '<cmd>Git blame<cr>');
-  end
+      -- Git blame
+      vim.keymap.set('n', '<leader>gb', '<cmd>Git blame<cr>');
+    end
+  },
+  {
+    'sindrets/diffview.nvim',
+    lazy = true,
+    keys = {
+      { '<leader>gd', '<cmd>DiffviewOpen<cr>',            desc = 'Diff view' },
+      { '<leader>gL', '<cmd>DiffviewFileHistory %<cr>',   desc = 'File history' },
+      { '<leader>gH', '<cmd>DiffviewFileHistory<cr>',     desc = 'Repo history' },
+      { '<leader>gx', '<cmd>DiffviewClose<cr>',           desc = 'Close diff view' },
+    },
+  },
 }
 
