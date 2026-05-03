ae7a6c6f7769110cd2a2c222b76bbfe4cb45ab10
Alexandra Babis <aebabis@gmail.com>
2026-05-03 00:47:24 -0700

    Removing integrated terminal

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -7,7 +7,13 @@ require('plugin.journal');
 require('plugin.treesitter');
 
 require('config.lazy')
-require('config.mappings')
+-- Correct priority for dockerfiles that end in an existing extension
+-- this allows `dockerfile.<commonExt> to resolve as a dockerfile for syntax highlighting
+vim.filetype.add({
+  pattern = {
+    ['Dockerfile%..*'] = { 'dockerfile', { priority = math.huge } }
+  }
+})
 require('config.autocmd')
 
 vim.cmd.colorscheme('nightfox')
