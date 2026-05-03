a447260433defe14e63e505a7d90abb2e9f13b2f
Alexandra Babis <aebabis@gmail.com>
2026-05-03 02:55:26 -0700

    Porting autocomplete hotkeys

--- nvim/.config/nvim/lua/plugin/autocomplete.lua
+++ nvim/.config/nvim/lua/plugin/autocomplete.lua
@@ -11,3 +11,42 @@ vim.api.nvim_create_autocmd('LspAttach', {
   end
 });
 
+local function scroll_docs(delta)
+  for _, win in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
+    if vim.api.nvim_win_get_config(win).relative ~= '' then
+      vim.api.nvim_win_call(win, function()
+        vim.fn.winrestview({ topline = math.max(1, vim.fn.winsaveview().topline + delta) })
+      end)
+      return true
+    end
+  end
+end
+
+vim.keymap.set('i', '<C-f>',     function() return scroll_docs(4)  and '' or '<C-f>' end, { expr = true })
+vim.keymap.set('i', '<C-b>',     function() return scroll_docs(-4) and '' or '<C-b>' end, { expr = true })
+vim.keymap.set('i', '<C-k>',     function() return vim.fn.pumvisible() == 1 and '<C-p>' or '<C-k>' end, { expr = true })
+vim.keymap.set('i', '<C-j>',     function() return vim.fn.pumvisible() == 1 and '<C-n>' or '<C-j>' end, { expr = true })
+vim.keymap.set('i', '<C-Space>', '<C-x><C-o>')
+
+vim.keymap.set({ 'i', 's' }, '<Tab>', function()
+  if vim.fn.pumvisible() == 1 then
+    return '<C-n>'
+  elseif vim.snippet.active({ direction = 1 }) then
+    vim.snippet.jump(1)
+    return ''
+  else
+    return '<Tab>'
+  end
+end, { expr = true })
+
+vim.keymap.set({ 'i', 's' }, '<S-Tab>', function()
+  if vim.fn.pumvisible() == 1 then
+    return '<C-p>'
+  elseif vim.snippet.active({ direction = -1 }) then
+    vim.snippet.jump(-1)
+    return ''
+  else
+    return '<S-Tab>'
+  end
+end, { expr = true })
+
